c23c3100139b35f31d02a599c7e69d1b4d08cf41
Emmanuelcookey15 <Emmanuelcookey744@gmail.com@users.noreply.github.com>
2020-11-24 11:54:11 +0100

    Currency converter fully set up, added the sign up text and icon, also the currency header text has also been added along with removal of the removal of the default app bar as indicated in the design.

---
+++ app/src/main/java/com/cookey/emmanuel/currencyconverter/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.cookey.emmanuel.currencyconverter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+
+    }
+}
