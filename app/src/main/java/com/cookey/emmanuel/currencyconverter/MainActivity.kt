4bc04462e6175f3623bd45816724c5e6fcd68033
Emmanuelcookey15 <Emmanuelcookey744@gmail.com@users.noreply.github.com>
2020-11-25 17:50:16 +0100

    Create a drawable for the EditText background, and added thrird party library for graphical representation of data as a lineChart

--- app/src/main/java/com/cookey/emmanuel/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/cookey/emmanuel/currencyconverter/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.cookey.emmanuel.currencyconverter
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mChart: LineChart
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +19,28 @@ class MainActivity : AppCompatActivity() {
         } catch (e: NullPointerException) {
         }
 
+        mChart = findViewById<LineChart>(R.id.chart)
+        mChart.setBackgroundColor(Color.TRANSPARENT)
+        mChart.setGridBackgroundColor(Color.WHITE)
+        mChart.setDrawGridBackground(true)
+
+
+        mChart.setDrawBorders(true)
+        mChart.description.isEnabled = false
+        mChart.setPinchZoom(false)
+
+        val legend = mChart.legend
+        legend.isEnabled = false
+
+
     }
+
+    private fun setData(count: Int, range: Float){
+
+        val yVals = ArrayList<Entry>()
+
+
+    }
+
+
 }
